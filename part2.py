188593584ba790b5491c12d13915ccb70c1f8ed9
kazantsevalexander <kazantsevalexander@gmail.com>
2024-10-25 22:26:23 +0300

    filtered Nan

--- part2.py
+++ part2.py
@@ -1,12 +1,10 @@
 import pandas as pd
 
 df = pd.read_csv('dz.csv')
-df.fillna(0, inplace=True)
-
-# Фильтруем строки, где Salary > 0
-df_filtered = df[df['Salary'] > 0]
-
 print(df)
 
+df.dropna(inplace=True)
+
+# Группируем по городу и вычисляем среднее значение зарплаты
 group = df.groupby('City')['Salary'].mean()
 print(group)
